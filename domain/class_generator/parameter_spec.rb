6d93c1e064216c9eec534e80faa51c54cce4a5fa
Taiga Miyazaki <mk2_tg000@icloud.com>
2020-03-07 23:47:58 +0900

    自動生成パラメータエンティティ実装

--- domain/class_generator/parameter_spec.rb
+++ domain/class_generator/parameter_spec.rb
@@ -3,4 +3,23 @@ require_relative '../entity'
 # パラメーター生成に必要な要素を持つエンティティ
 class ParameterSpec
   include Entity
+
+  attr_reader :parameter_name,
+              :decorator,
+              :type
+
+  # 引数は文字列で頼む
+  def set_parameter(name)
+    @parameter_name = name
+  end
+
+  # 引数は文字列で頼む
+  def set_decorator(decorator)
+    @decorator = decorator
+  end
+
+  # 引数は文字列で頼む
+  def set_type(type)
+    @type = type
+  end
 end
