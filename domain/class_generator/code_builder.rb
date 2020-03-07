9aa02d75c2cfcf9fd0e7f8bd2574a440471da469
Taiga Miyazaki <mk2_tg000@icloud.com>
2020-03-07 23:48:16 +0900

    自動書き出し処理に関連するクラス実装

--- domain/class_generator/code_builder.rb
+++ domain/class_generator/code_builder.rb
@@ -1,3 +1,12 @@
 # コード生成情報を持つクラス
 class CodeBuilder
+  
+  attr_reader :class_spec,
+              :create_path
+
+  def initialize(class_spec, create_path)
+    @class_spec = class_spec
+    @create_path = create_path
+  end
+
 end
--- domain/code_writer.rb
+++ domain/code_writer.rb
@@ -1,3 +1,8 @@
 # コードを書き出すモジュール
 module CodeWriter
+
+  def write(code_builder)
+    raise "#{self.class}##{__method__}が実装されていません"
+  end
+
 end
